ef5232b859d68afb3efe81998802d3b4912112cb
Leo0114 <leonel0614@gmail.com>
2026-04-15 11:09:45 -0600

    update: sitemap & robots

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,10 +7,24 @@ import tailwindcss from '@tailwindcss/vite';
 
 import icon from "astro-icon";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://homii.net",
-  integrations: [react(), icon()],
+  integrations: [
+    react(),
+    icon(),
+    sitemap({
+      i18n: {
+        defaultLocale: "es",
+        locales: {
+          es: "es-MX",
+          en: "en-US",
+        },
+      },
+    }),
+  ],
 
   vite: {
     plugins: [tailwindcss()]
